e94da593a9702dbdcfee26952f00886939843a63
kadirsefaunal <kadir-unal@outlook.com>
2016-10-29 23:06:21 +0300

    Düzenleme yapıldı.

--- RomaliAskerProblemi/RomalıAsker/Program.cs
+++ RomaliAskerProblemi/RomalıAsker/Program.cs
@@ -10,43 +10,73 @@ namespace RomalıAsker
     {
         static void Main(string[] args)
         {
+            int kSayisi = 0, aSayisi = 0;
+
             Console.Write("Kişi sayısını girin: ");
-            int kisiSayisi = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Adım sayısını girin: ");
-            int adinSayisi = Convert.ToInt32(Console.ReadLine());
-            int[] kisiler = new int[kisiSayisi];
-            for (int i = 0; i < kisiSayisi; i++)
+            string kisiSayisi = Console.ReadLine();
+            if (SayiMi.Kontrol(kisiSayisi))
             {
-                kisiler[i] = 1;
-            }
+                kSayisi = Convert.ToInt32(kisiSayisi);
 
-            int oluSayisi = 0, j = -1, k = 0;
-            while (oluSayisi != (kisiSayisi - 2))
-            {
-                while (k != adinSayisi)
+                Console.Write("Adım sayısını girin: ");
+                string adimSayisi = Console.ReadLine();
+                if (SayiMi.Kontrol(adimSayisi))
                 {
-                    j++;
-                    if (j == kisiSayisi)
-                        j = 0;
-                    if (kisiler[j] == 1)
-                        k++;
-                }
-                kisiler[j] = 0;
-                oluSayisi++;
-                k = 0;
-                for (int i = 0; i < kisiSayisi; i++)
-                {
-                    Console.Write(kisiler[i] + " - ");
-                }
-                Console.WriteLine();
-            }
+                    aSayisi = Convert.ToInt32(adimSayisi);
+                    if (kSayisi > 1 && aSayisi > 0)
+                    {
+                        int[] kisiler = new int[kSayisi];
+                        for (int i = 0; i < kSayisi; i++)
+                            kisiler[i] = 1;
 
-            for (int i = 0; i < kisiSayisi; i++)
-            {
-                if (kisiler[i] == 1)
-                    Console.WriteLine(i + 1);
+                        int oluSayisi = 0, j = -1, k = 0;
+                        while (oluSayisi != (kSayisi - 2))
+                        {
+                            while (k != aSayisi)
+                            {
+                                j++;
+                                if (j == kSayisi)
+                                    j = 0;
+                                if (kisiler[j] == 1)
+                                    k++;
+                            }
+                            kisiler[j] = 0;
+                            oluSayisi++;
+                            k = 0;
+                            for (int i = 0; i < kSayisi; i++)
+                            {
+                                Console.Write(kisiler[i] + " - ");
+                            }
+                            Console.WriteLine();
+                        }
+
+                        for (int i = 0; i < kSayisi; i++)
+                        {
+                            if (kisiler[i] == 1)
+                                Console.WriteLine(i + 1);
+                        }
+                    }
+                    else
+                        Console.WriteLine("Kişi sayısı 1'den fazla olmalı yada adım sayısı 0'dan fazla olmalı!");
+                }
+                else
+                    Console.WriteLine("Adim sayısı hatalı!");
             }
+            else
+                Console.WriteLine("Kisi sayısı hatalı!");
             Console.ReadKey();
+        }
+
+        static class SayiMi
+        {
+            public static bool Kontrol(string ifade)
+            {
+                foreach (char chr in ifade)
+                {
+                    if (!Char.IsNumber(chr)) return false;
+                }
+                return true;
+            }
         }
     }
 }
