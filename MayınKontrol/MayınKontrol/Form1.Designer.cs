9951729459d806db09404ddeab43881cc1f5a1e4
kadirsefaunal <kadir-unal@outlook.com>
2016-10-31 12:38:03 +0300

    Mayın Kontrol uygulamasının dosyaları eklendi.

---
+++ MayınKontrol/MayınKontrol/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MayınKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.lblEn = new System.Windows.Forms.Label();
+            this.lblBoy = new System.Windows.Forms.Label();
+            this.txtEn = new System.Windows.Forms.TextBox();
+            this.txtBoy = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Location = new System.Drawing.Point(210, 12);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(75, 23);
+            this.btnOlustur.TabIndex = 0;
+            this.btnOlustur.Text = "Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(291, 12);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(75, 23);
+            this.btnBasla.TabIndex = 1;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            // 
+            // lblEn
+            // 
+            this.lblEn.AutoSize = true;
+            this.lblEn.Location = new System.Drawing.Point(15, 17);
+            this.lblEn.Name = "lblEn";
+            this.lblEn.Size = new System.Drawing.Size(26, 13);
+            this.lblEn.TabIndex = 2;
+            this.lblEn.Text = "En: ";
+            // 
+            // lblBoy
+            // 
+            this.lblBoy.AutoSize = true;
+            this.lblBoy.Location = new System.Drawing.Point(113, 17);
+            this.lblBoy.Name = "lblBoy";
+            this.lblBoy.Size = new System.Drawing.Size(31, 13);
+            this.lblBoy.TabIndex = 3;
+            this.lblBoy.Text = "Boy: ";
+            // 
+            // txtEn
+            // 
+            this.txtEn.Location = new System.Drawing.Point(47, 14);
+            this.txtEn.Name = "txtEn";
+            this.txtEn.Size = new System.Drawing.Size(42, 20);
+            this.txtEn.TabIndex = 4;
+            // 
+            // txtBoy
+            // 
+            this.txtBoy.Location = new System.Drawing.Point(150, 14);
+            this.txtBoy.Name = "txtBoy";
+            this.txtBoy.Size = new System.Drawing.Size(42, 20);
+            this.txtBoy.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 420);
+            this.Controls.Add(this.txtBoy);
+            this.Controls.Add(this.txtEn);
+            this.Controls.Add(this.lblBoy);
+            this.Controls.Add(this.lblEn);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.btnOlustur);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label lblEn;
+        private System.Windows.Forms.Label lblBoy;
+        private System.Windows.Forms.TextBox txtEn;
+        private System.Windows.Forms.TextBox txtBoy;
+    }
+}
+
---
+++ MayınKontrol/MayınKontrol/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayınKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Button[,] butonlar;
+        int en, boy;
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            en = Convert.ToInt32(txtEn.Text);
+            boy = Convert.ToInt32(txtBoy.Text);
+            butonlar = new Button[en, boy];
+            Button btn;
+
+            for (int i = 0; i < en; i++)
+            {
+                for (int j = 0; j < boy; j++)
+                {
+                    btn = new Button();
+                    btn.Name = "btn" + i + "_" + j;
+                    btn.Width = 30;
+                    btn.Height = 30;
+                    btn.Top = 50 + (i * 30);
+                    btn.Left = 10 + (j * 30);
+                    btn.FlatStyle = FlatStyle.Flat;
+
+                    if (i == 0 || j == 0 || i == (en - 1) || j == (boy - 1))
+                        btn.BackColor = Color.Gray;
+                    else
+                        btn.Click += Btn_Click;
+                    butonlar[i, j] = btn;
+                }
+            }
+
+            foreach (var item in butonlar)
+            {
+                Controls.Add(item);
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (sender as Button);
+            MessageBox.Show("Test");
+        }
+    }
+}
