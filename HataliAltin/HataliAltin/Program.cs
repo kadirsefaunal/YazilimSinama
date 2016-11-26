4e4a9a1c27ad0c8ef2a668ad6ff97ac515ee0594
kadirsefaunal <kadir-unal@outlook.com>
2016-11-26 23:13:33 +0300

    Hatalı altın problemi tamamlandı. Kontrollerin eklenmesi gerekiyor.

--- HataliAltin/HataliAltin/Program.cs
+++ HataliAltin/HataliAltin/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace HataliAltin
 {
@@ -10,7 +7,33 @@ namespace HataliAltin
     {
         static void Main(string[] args)
         {
-            
+            Stopwatch sure = new Stopwatch(); //Geçen süreyi bulmak için kullanılıyor.
+
+            Console.Write("Altın Sayısını Girin: ");
+            int altinSayisi = Convert.ToInt32(Console.ReadLine());
+
+            int[] altinlar = new int[altinSayisi];
+            for (int i = 0; i < altinSayisi; i++)
+                altinlar[i] = 1;
+
+            Random rast = new Random();
+            int hataliAltinIndis = rast.Next(0, altinSayisi);
+            altinlar[hataliAltinIndis] = 0;
+
+            Console.WriteLine();
+            foreach (int item in altinlar)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Terazi terazi = new Terazi();
+            sure.Start();
+            terazi.Hesapla(altinlar, altinSayisi);
+            sure.Stop();
+
+            Console.WriteLine("\nÇalisma süresi: " + sure.Elapsed.TotalMilliseconds + " milisaniye\n");
+            Console.ReadKey();
         }
     }
 }
---
+++ HataliAltin/HataliAltin/Terazi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HataliAltin
+{
+    class Terazi
+    {
+        private int indis { get; set; }
+        private int adimSayisi { get; set; }
+
+        public void Hesapla(int[] dizi, int altinSayisi)
+        {
+            Hesapla(dizi, 0, altinSayisi);
+        }
+
+        private void Hesapla(int[] dizi, int baslangic, int bitis)
+        {
+            adimSayisi++;
+            int solToplam = 0, sagToplam = 0;
+            int x = 0;
+
+            if ((bitis - baslangic) % 2 == 0)
+            {
+                x = (bitis - baslangic) / 2;
+            }
+            else
+            {
+                x = (bitis - 1 - baslangic) / 2;
+                indis = bitis;
+                bitis--;
+            }
+
+            for (int i = baslangic; i < (baslangic + x); i++)
+                solToplam += dizi[i];
+            for (int i = (baslangic + x); i < bitis; i++)
+                sagToplam += dizi[i];
+
+            if (solToplam > sagToplam)
+                Hesapla(dizi, (baslangic + x), bitis);
+            else if (solToplam < sagToplam)
+                Hesapla(dizi, baslangic, (baslangic + x));
+            else
+            {
+                Console.WriteLine("Hatalı altın: " + indis);
+                Console.WriteLine("Tartılma sayısı: " + adimSayisi);
+            }
+                
+        }
+    }
+}
