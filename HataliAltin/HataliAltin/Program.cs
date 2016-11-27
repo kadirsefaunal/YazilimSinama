106dbd6a48a6013b887dc30918592c7cc7a662e5
Kadir Mutlu <kadirmutluu@gmail.com>
2016-11-27 12:45:24 +0300

    Kontrol eklendi.

--- HataliAltin/HataliAltin/Program.cs
+++ HataliAltin/HataliAltin/Program.cs
@@ -7,33 +7,54 @@ namespace HataliAltin
     {
         static void Main(string[] args)
         {
+            int aSayisi = 0;
             Stopwatch sure = new Stopwatch(); //Geçen süreyi bulmak için kullanılıyor.
 
             Console.Write("Altın Sayısını Girin: ");
-            int altinSayisi = Convert.ToInt32(Console.ReadLine());
+            string altinSayisi = Console.ReadLine();
 
-            int[] altinlar = new int[altinSayisi];
-            for (int i = 0; i < altinSayisi; i++)
-                altinlar[i] = 1;
-
-            Random rast = new Random();
-            int hataliAltinIndis = rast.Next(0, altinSayisi);
-            altinlar[hataliAltinIndis] = 0;
-
-            Console.WriteLine();
-            foreach (int item in altinlar)
+            if (SayiMi.Kontrol(altinSayisi))
             {
-                Console.Write(item + " ");
+                aSayisi = Convert.ToInt32(altinSayisi);
+
+                int[] altinlar = new int[aSayisi];
+                for (int i = 0; i < aSayisi; i++)
+                    altinlar[i] = 1;
+
+                Random rast = new Random();
+                int hataliAltinIndis = rast.Next(0, aSayisi);
+                altinlar[hataliAltinIndis] = 0;
+
+                Console.WriteLine();
+                foreach (int item in altinlar)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+
+                Terazi terazi = new Terazi();
+                sure.Start();
+                terazi.Hesapla(altinlar, aSayisi);
+                sure.Stop();
+
+                Console.WriteLine("\nÇalisma süresi: " + sure.Elapsed.TotalMilliseconds + " milisaniye\n");
+                Console.ReadKey();
             }
-            Console.WriteLine();
-
-            Terazi terazi = new Terazi();
-            sure.Start();
-            terazi.Hesapla(altinlar, altinSayisi);
-            sure.Stop();
-
-            Console.WriteLine("\nÇalisma süresi: " + sure.Elapsed.TotalMilliseconds + " milisaniye\n");
+            else
+                Console.WriteLine("Bu değer girilemez!");
             Console.ReadKey();
+        }
+
+        static class SayiMi
+        {
+            public static bool Kontrol(string ifade)
+            {
+                foreach (char chr in ifade)
+                {
+                    if (!Char.IsNumber(chr)) return false;
+                }
+                return true;
+            }
         }
     }
 }
